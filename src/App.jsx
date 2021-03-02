77f298cfa7dc419ab9e0349005b6470497d7a616
ethanjd23 <ethanjd23@gmail.com>
2021-03-02 10:43:45 -0600

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+
+
+    return (
+        <>
+            <h1 className="display-1 bg-success m-3">she workin</h1>
+        </>
+    )
+}
+
+export default App;
